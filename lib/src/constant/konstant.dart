268f46304488d08fe87baf4d79037804bcf96613
Monday Solomon <mondaysolomon01@gmail.com>
2023-05-06 21:51:19 +0100

    update unecessary 👨‍🍳

---
+++ lib/src/constant/konstant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+
+@immutable
+class AuthKonstant {
+  static const accountExistsWithDifferentCredential = 'accounts-exists-with-different-credential';
+  static const googleCom = "google.com";
+  static const emailScope = "email";
+
+
+  const AuthKonstant._();
+}
+
---
+++ lib/src/features/authentication/data/authenticator.dart
@@ -0,0 +1,9 @@
+
+class Authenticator{
+ const Authenticator(); 
+
+
+
+  Future
+
+}
